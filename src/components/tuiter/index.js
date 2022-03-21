553bcdfc27e86bd418f3afa98363d07c162e22b9
Amal Tharyan <amal@Amals-MacBook-Air.local>
2022-03-21 19:48:15 -0400

    Take off unnecessary Link component

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import NavigationSidebar from "./NavigationSidebar";
 import WhoToFollowList from "./WhoToFollowList";
 import ExploreComponent from './ExploreScreen/ExploreComponent';
