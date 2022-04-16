7bd6ee14cc16eb591093f26c6b04e79fa991bc5b
yomal148 <amaltharyan@gmail.com>
2022-04-15 21:27:46 -0400

    a9 MONGO sp22

--- src/components/tuiter/services/tuits-service.js
+++ src/components/tuiter/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
-const TUITS_API = `http://localhost:4000/api/tuits`;
+
+const TUITS_API = 'http://localhost:4000/api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit);
